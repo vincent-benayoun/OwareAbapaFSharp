e885f9fd2f386d392eb412fe8f3b3e86a7f29110
Meirb <Meirb@MEIRB-LAPTOP>
2018-02-20 18:35:01 +0200

    add GameController and Board model

---
+++ OwareAbapa/Controllers/GameController.fs
@@ -0,0 +1,10 @@
+﻿namespace OwareAbapa.Controllers
+open System.Web.Http
+
+type GameController() =
+    inherit ApiController()
+
+    let values = [| Board.initial |]
+
+    /// Gets all values.
+    member x.Get() = values
