ddb4112a574491654665b644cff6f59c84db2f83
Meirb <Meirb@MEIRB-LAPTOP>
2018-02-20 18:03:21 +0200

    Add project files.

---
+++ OwareAbapa/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Inherits="OwareAbapa.Global" %>
---
+++ OwareAbapa/Models/Car.fs
@@ -0,0 +1,9 @@
+namespace OwareAbapa.Models
+
+open Newtonsoft.Json
+
+[<CLIMutable>]
+type Car = {
+    Make : string
+    Model : string
+}
