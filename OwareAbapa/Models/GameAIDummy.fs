aadd5ebb0f00874606467989c3ccb4f5f5813b48
Meirb <Meirb@MEIRB-LAPTOP>
2018-02-25 10:03:50 +0200

    dummy AI implementation

---
+++ OwareAbapa/Models/GameAIDummy.fs
@@ -0,0 +1,24 @@
+﻿namespace OwareAbapa.Models
+
+open GameState
+
+module GameAIDummy =
+
+    let gameStateAfterPlaying gameState case =
+        match GameLogic.play gameState case with
+        | GameLogic.Played (gameStateAfter) -> [(case, gameStateAfter)]
+        | GameLogic.InvalidCase
+        | GameLogic.EmptyCase
+        | GameLogic.NeedsToFeed
+        | GameLogic.InternalError -> []
+
+    let nextGameStates gameState =
+        let casesOfPlayer = Board.casesOfPlayer gameState.currentPlayer
+        List.collect (gameStateAfterPlaying gameState) casesOfPlayer
+
+    let play gameState =
+        let nextGameStatesList = nextGameStates gameState
+        let random = new System.Random()
+        let randomNextGameStateIndex = random.Next(List.length nextGameStatesList)
+        let caseToPlay = fst (List.nth nextGameStatesList randomNextGameStateIndex)
+        caseToPlay
---
+++ OwareAbapa/Models/GameLogic.fsi
@@ -0,0 +1,9 @@
+﻿namespace OwareAbapa.Models
+
+module GameLogic =
+
+    type distribute_result = InvalidArgument | Distrubuted of Board.board * Board.case
+    
+    type play_result = InvalidCase | EmptyCase | NeedsToFeed | InternalError | Played of GameState.game_state
+    val play: GameState.game_state -> Board.case -> play_result
+    
